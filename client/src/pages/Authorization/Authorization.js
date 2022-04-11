ba98eef03a9550aa009909f7b3a691c28119c85e
Erinn Schneider <erinnaschneider@gmail.com>
2022-04-11 19:45:13 -0400

    adding authorization pages

---
+++ client/src/pages/Authorization/Authorization.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../../components/Login/Login";
+import Signup from "../../components/Signup/Signup";
+
+const Authorization = () => {
+  return (
+    <div>
+      <Login />
+      <Signup />
+    </div>
+  );
+};
+
+export default Authorization;
